3c459d8c6cefee0aff5b79c7d34e3c9d39d39329
B9DF <B9DF@MI00323961.petrobras.biz>
2017-02-01 12:54:45 -0200

    Findcomponent pelo id, JSF

---
+++ Exemplos/Codes/FindComponentJSF.java
@@ -0,0 +1,30 @@
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIViewRoot;
+import javax.faces.component.visit.VisitCallback;
+import javax.faces.component.visit.VisitContext;
+import javax.faces.component.visit.VisitResult;
+import javax.faces.context.FacesContext;
+
+import com.sun.faces.component.visit.FullVisitContext;
+
+public static void main(String[] args) {
+
+	private UIInput findComponent(final String id) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		UIViewRoot root = context.getViewRoot();
+		final UIComponent[] found = new UIComponent[1];
+
+		root.visitTree(new FullVisitContext(context), new VisitCallback() {
+			@Override
+			public VisitResult visit(VisitContext context, UIComponent component) {
+				if (component.getId().equals(id)) {
+					found[0] = component;
+					return VisitResult.COMPLETE;
+				}
+				return VisitResult.ACCEPT;
+			}
+		});
+		return (UIInput) found[0];
+	}
+}
